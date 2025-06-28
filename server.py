f8e6479c89af27c4541679a723d4d447d7e18a74
ab ya <abstyle0701@gmail.com>
2025-06-28 13:55:15 +0530

    server update

--- server.py
+++ server.py
@@ -11,7 +11,21 @@ app = FastAPI()
 # Include book routes
 app.include_router(book_router)
 
-# Run with: python server.py
+@app.get("/")
+def testserver():
+    return {"status" :200,"details":"server is running"}
+
+# # In root directory 
+
+# if you migrating, this will run the initial migration
+# You can run the following command to create the initial migration:
+# alembic revision --autogenerate -m "Initial tables and index"
+# and then apply it with:
+# alembic upgrade head
+
+# To run the server, you can use the command below.
+# uvicorn server:app --reload or python server.py
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
